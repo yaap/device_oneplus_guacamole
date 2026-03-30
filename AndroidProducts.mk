9b109c34591e2724c95be4d7689ff561ea0b5899
Zhouxulin95 <71020249+Zhouxulin95@users.noreply.github.com>
2026-03-30 16:55:37 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/yaap_guacamole.mk
+    $(LOCAL_DIR)/custom_guacamole.mk
 
 COMMON_LUNCH_CHOICES := \
-	yaap_guacamole-user \
-	yaap_guacamole-userdebug \
-	yaap_guacamole-eng
+	custom_guacamole-user \
+	custom_guacamole-userdebug \
+	custom_guacamole-eng
